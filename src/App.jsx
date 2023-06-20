6baa2fe3d7391896e922865413be4b8baaeac788
Abián <abian.camejo@gmail.com>
2023-06-20 17:11:23 +0100

    in process of mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bulma/css/bulma.min.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      SPLIT 
+      <br/>
+      TTER
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+     <div className="hero has-background-white">
+      <div className="columns has-text-left" style={{ width:"100%", marginLeft:"40px", marginTop:"20px"}}>
+        Bill
+        <div className='column has-background-black' style={{width:"80%", height:"40px", marginTop:"5px"}}>
+
+        </div>
+        <div className='container' style={{marginTop:"25px"}}>
+        Select Tip %
+        <div className='column'>5</div>
+        <div className='column'>5</div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+ 
+</div>
+
     </>
-  )
+    )
 }
 
 export default App
